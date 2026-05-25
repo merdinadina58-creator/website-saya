e472d7bce41599a957a5db726dd47948c4c3f174
Z User <z@container>
2026-05-25 20:09:07 +0000

    27ffacb8-139f-45ae-a4bb-74d28f83ca20

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-# Keep-alive script that restarts the dev server when it dies
-cd /home/z/my-project
-
 while true; do
-  echo "[$(date)] Starting dev server..."
-  bun run dev > /home/z/my-project/dev.log 2>&1
-  SERVER_PID=$!
-  echo "[$(date)] Server started with PID $SERVER_PID"
-
-  # Wait for the server to die
-  wait $SERVER_PID 2>/dev/null
-  EXIT_CODE=$?
-  echo "[$(date)] Server exited with code $EXIT_CODE, restarting in 3s..."
-  sleep 3
+  node .next/standalone/server.js
+  sleep 2
 done
